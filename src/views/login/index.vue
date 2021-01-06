6cf4256550ff41eeac2eb65ecdc2609c7bf38a67
jkchen-zj <1547093060@qq.com>
2021-01-06 22:14:43 +0800

    1/6

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="login-contain">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-            <el-form-item label="密码" prop="pass">
-                <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            <el-form-item  prop="user">
+                <span class="icon-contain">
+                  <i class="el-icon-user-solid"></i>
+                </span>
+                <el-input type="text" v-model="ruleForm.user" autocomplete="off"></el-input>
             </el-form-item>
-            <el-form-item label="确认密码" prop="checkPass">
-                <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            <el-form-item  prop="password">
+                <span class="icon-contain">
+                  <i class="el-icon-lock"></i>
+                </span>
+                <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
             </el-form-item>
-            <el-form-item label="年龄" prop="age">
-                <el-input v-model.number="ruleForm.age"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+            <el-form-item class="loginForm">
+                <el-button type="primary" @click="submitForm('ruleForm')" class="thirdparty-button">Enter the system</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -20,22 +23,6 @@
 <script>
 export default {
     data() {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
       var validatePass = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请输入密码'));
@@ -46,30 +33,24 @@ export default {
           callback();
         }
       };
-      var validatePass2 = (rule, value, callback) => {
+      var user = (rule, value, callback) => {
         if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
+          callback(new Error('请输入账户'));
+        }else {
           callback();
         }
       };
       return {
         ruleForm: {
-          pass: '',
-          checkPass: '',
-          age: ''
+          password: '',
+          user:'',
         },
         rules: {
-          pass: [
+          password: [
             { validator: validatePass, trigger: 'blur' }
           ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
+          user: [
+            { validator: user, trigger: 'blur' }
           ]
         }
       };
@@ -95,5 +76,31 @@ export default {
         width: 100%;
         background-color: #2d3a4b;
         overflow: hidden;
+        .demo-ruleForm{
+          width: 500px;
+          margin: 0px auto;
+          margin-top: 300px;
+        }
+        .icon-contain{
+          width: 30px;
+          height: 100%;
+          display: inline-block;
+          padding: 5px 5px 5px 15px;
+          i{
+            font-size: 20px;
+          }
+        }
+        .el-input{
+          width: 85%;
+        }
+      .thirdparty-button{
+        border: none;
+        border-radius: 0px;
+        background: linear-gradient(45deg,transparent 5%,#ff013c 5%);
+        color: #fff;
+        letter-spacing: 3px;
+        box-shadow: 6px 0px 0px #00E6F6;
+        outline: transparent;
+      }
     }
 </style>
