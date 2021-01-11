078b7c48067c30bc2dfc6c1b5ce3f7e071b0a2a2
jkchen-zj <1547093060@qq.com>
2021-01-11 23:11:50 +0800

    1/11

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,36 @@
 <template>
     <div class="login-contain">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-            <el-form-item  prop="user">
-                <span class="icon-contain">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm"  class="demo-ruleForm">
+           <div class="title-container">
+            <h3 class="title">Man HuaTai</h3>
+          </div>
+
+           <el-form-item prop="username" class="">
+             <span class="icon-contain">
                   <i class="el-icon-user-solid"></i>
                 </span>
-                <el-input type="text" v-model="ruleForm.user" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item  prop="password">
-                <span class="icon-contain">
-                  <i class="el-icon-lock"></i>
-                </span>
-                <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item class="loginForm">
-                <el-button type="primary" @click="submitForm('ruleForm')" class="thirdparty-button">Enter the system</el-button>
-            </el-form-item>
+            <el-input
+              ref="username"
+              v-model="ruleForm.username"
+              placeholder="Username"
+              name="username"
+              type="text"
+              tabindex="1"
+              autocomplete="on"
+            />
+          </el-form-item>
+
+          <el-form-item  prop="password">
+              <span class="icon-contain">
+                <i class="el-icon-lock"></i>
+              </span>
+              <el-input type="password" placeholder="Password" v-model="ruleForm.password" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item class="loginForm ct">
+              <div class="thirdparty-button">Enter the system</div>
+              <!-- <el-button type="primary" @click="submitForm('ruleForm')" class="thirdparty-button">Enter the system</el-button> -->
+          </el-form-item>
         </el-form>
     </div>
 </template>
@@ -33,7 +48,7 @@ export default {
           callback();
         }
       };
-      var user = (rule, value, callback) => {
+      var username = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请输入账户'));
         }else {
@@ -43,14 +58,14 @@ export default {
       return {
         ruleForm: {
           password: '',
-          user:'',
+          username:'',
         },
         rules: {
           password: [
             { validator: validatePass, trigger: 'blur' }
           ],
-          user: [
-            { validator: user, trigger: 'blur' }
+          username: [
+            { validator: username, trigger: 'blur' }
           ]
         }
       };
@@ -70,16 +85,71 @@ export default {
 }
 </script>
 
+// reset element
+<style lang="scss">
+input:-webkit-autofill {
+  box-shadow: 0 0 0px 1000px rgba(54,71,130,0.1) inset !important;
+
+  -webkit-text-fill-color: #fff !important;
+
+  -webkit-background-clip:text;
+
+}
+  .login-contain{
+    .el-form-item__error{
+      left: 50px;
+    }
+    .el-input{
+      .el-form-item{
+       
+      }
+      input{
+        color: #fff;
+        overflow: hidden;
+        background-color: transparent;
+        border: 1px solid  rgba(255,255,255,0.4);
+      }
+    }
+  }
+  .el-button--primary{
+    padding: 0px;
+  }
+</style>
+
 <style lang="scss" scoped>
     .login-contain{
         min-height: 100%;
         width: 100%;
-        background-color: #2d3a4b;
         overflow: hidden;
+        position: relative;
+        &:after{
+          content: '';
+          display: block;
+          position: absolute;
+          top: 0px;
+          left: 0;
+          width: 102%;
+          height: 103%;
+          margin-left: -1%;
+          margin-top: -1%;
+          background-image: url('../../../public/login/backgroud.jpg');
+          background-repeat: no-repeat;
+          background-size: 100%;
+          z-index: -1;
+          filter: blur(6px);
+        }
+
         .demo-ruleForm{
-          width: 500px;
+          width: 450px;
           margin: 0px auto;
-          margin-top: 300px;
+          margin-top: 180px;
+          padding: 15px 10px;
+          border-radius: 4px;
+          background: linear-gradient(90deg, rgba(255,255,255,0.2), rgba(255,255,255,0.0));
+          // border: 1px solid  rgba(255,255,255,0.2);
+          .title-container{
+            font-family: 'Rayleigh';
+          }
         }
         .icon-contain{
           width: 30px;
@@ -88,19 +158,124 @@ export default {
           padding: 5px 5px 5px 15px;
           i{
             font-size: 20px;
+            color: #fff;
           }
         }
         .el-input{
           width: 85%;
         }
-      .thirdparty-button{
+      .thirdparty-button,.thirdparty-button:after{
+        width: 180px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        position: relative;
         border: none;
+        font-size: 16px;
         border-radius: 0px;
         background: linear-gradient(45deg,transparent 5%,#ff013c 5%);
         color: #fff;
         letter-spacing: 3px;
-        box-shadow: 6px 0px 0px #00E6F6;
+        box-shadow: 4px 0px 0px #00E6F6;
         outline: transparent;
+        font-family: 'BebasNeue',cursive;
+      }
+      .thirdparty-button:after{
+        --slice-0:inset(50% 50% 50% 50%);
+        --slice-1:inset(80% -6px 0 0);
+        --slice-2:inset(50% -6px 30% 0);
+          // translate(-10px, 15px);
+
+        --slice-3:inset(10% -6px 85% 0);
+        --slice-4:inset(40% -6px 43% 0);
+        --slice-5:inset(80% -3px 5% 0);
+
+        content: 'Enter the system';
+        display: block;
+        position: absolute;
+        top:0;
+        left: 0;
+        bottom: 0;
+        background: linear-gradient(45deg,transparent 3%,#00E6F6 3%,#00E6F6 5%,#ff013c 5%);
+        text-shadow: -1.5px -1.5px 0px #f8f005, 1.5px 1.5px 0px #00e6f6;
+        clip-path: var(--slice-0);
+        transform: translate(-10px,5px);
+      }
+      .thirdparty-button:hover:after{
+        animation:glitch 1s;
+        animation-timing-function: steps(2,end);
+      }
+
+      .title{
+        text-align: center;
+        color: #fff;
+        font-size: 55px;
+        margin-bottom: 30px;
       }
     }
+
+
+
+
+
+
+
+
+@keyframes glitch {
+  0% {
+    clip-path: var(--slice-1);
+   transform: translate(-10px,5px);
+  }
+  
+  10% {
+    clip-path: var(--slice-3);
+    transform: translate(0px,10px);
+  }
+  
+  20% {
+    clip-path: var(--slice-1);
+    transform: translate(-5px,5px);
+  }
+  
+  30% {
+    clip-path: var(--slice-3);
+    transform: translate(5px,10px);
+  }
+  
+  40% {
+    clip-path: var(--slice-2);
+    transform: translate(0px,-15px);
+  }
+  
+  50% {
+    clip-path: var(--slice-3);
+    transform: translate(10px,20px);
+  }
+  
+  60% {
+    clip-path: var(--slice-4);
+    transform: translate(-5px,10px);
+  }
+  
+  70% {
+    clip-path: var(--slice-2);
+  transform: translate(0px,0px);
+  }
+  
+  80% {
+    clip-path: var(--slice-5);
+    transform: translate(-10px,-20px);
+  }
+  
+  90% {
+    clip-path: var(--slice-1);
+    transform: translate(5px,0px);
+  }
+  
+  100% {
+    clip-path: var(--slice-1);
+    transform: translate(0);
+  }
+}
+
 </style>
